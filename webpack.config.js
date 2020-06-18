0e526b3c422255240186ee9d489a4003df19bdad
unknown <zcvfcat@gmail.com>
2020-06-19 00:18:29 +0900

    v

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/js/main.js'],
+  entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
   },
+  plugins: [new MiniCssExtractPlugin({ filename: 'css/style.css' })],
   module: {
     rules: [
       {
         test: /\.js$/,
         include: [path.resolve(__dirname, 'src/js')],
-        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -19,10 +20,15 @@ module.exports = {
             plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
       },
     ],
   },
   devtool: 'source-map',
-  // https://webpack.js.org/concepts/mode/#mode-development
   mode: 'development',
 }
